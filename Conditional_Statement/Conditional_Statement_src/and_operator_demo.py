d9aab1521ed130808cbd818ea7d792703b761ffd
CHEN WEI-CHENG <3331363@gmail.com>
2026-05-30 08:01:12 +0800

    20260530

---
+++ Conditional_Statement/Conditional_Statement_src/and_operator_demo.py
@@ -0,0 +1,6 @@
+score = 85
+attendance = 90
+
+result = score >= 60 and attendance >= 80
+
+print(result)
---
+++ Conditional_Statement/Conditional_Statement_src/discount_demo.py
@@ -0,0 +1,6 @@
+is_student = False
+has_coupon = True
+
+can_discount = is_student or has_coupon
+
+print(can_discount)
